adf5a645da1e5eeb6d8ba1a09562889749f26574
Alexandr Dunets <dunetsa@yandex.ru>
2026-01-04 00:24:16 +0300

    Small fixed

--- h_task/src/widget.py
+++ h_task/src/widget.py
@@ -1,7 +1,7 @@
-from masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_card_number, get_mask_account
 
 
-def ask_account_card(account_card_str="") -> str:
+def ask_account_card(account_card_str: str = "") -> str:
     """Функция принимает один аргумент — строку, содержащую тип и номер карты
     или счета, а возвращает строку с замаскированным номером."""
 
@@ -20,14 +20,14 @@ def ask_account_card(account_card_str="") -> str:
     return account_card_mask
 
 
-def get_date(date_in="") -> str:
+def get_date(date_in: str = "") -> str:
     """Функция get_date(date_in: str) принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
     и возвращает строку с датой в формате "ДД.ММ.ГГГГ" ("11.03.2024")."""
 
     date_out = ""
     if date_in:
-        day_dd = date_in[8: 10]
-        month_mm = date_in[5: 7]
+        day_dd = date_in[8:10]
+        month_mm = date_in[5:7]
         year_yyyy = date_in[:4]
         if day_dd.isdigit() and month_mm.isdigit() and year_yyyy.isdigit():
             date_out = f"{day_dd}.{month_mm}.{year_yyyy}"
