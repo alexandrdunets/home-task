2d446f6c6fef67d36ce98a2db2eb1caab9bc5270
Alexandr Dunets <dunetsa@yandex.ru>
2026-01-04 00:01:42 +0300

    Добавлен код для тестирования

--- h_task/main.py
+++ h_task/main.py
@@ -1,7 +1,9 @@
-from src.widget import ask_account_card
+from src.widget import ask_account_card, get_date
 
 
 def main():
+    """Проверка работы функций"""
+
     print(ask_account_card("Maestro 1596837868705199"))
     print(ask_account_card("Счет 64686473678894779589"))
     print(ask_account_card("MasterCard 7158300734726758"))
@@ -10,6 +12,7 @@ def main():
     print(ask_account_card("Visa Platinum 8990922113665229"))
     print(ask_account_card("Visa Gold 5999414228426353"))
     print(ask_account_card("Счет 73654108430135874305"))
+    print(get_date("2024-03-11T02:26:18.671407"))
 
 
 if __name__ == "__main__":
