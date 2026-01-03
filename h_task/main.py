4239616ac6bea009bb6d5262ec29e9bc5769721a
Alexandr Dunets <dunetsa@yandex.ru>
2026-01-03 22:56:52 +0300

    Инициализация функции main() для проверки

---
+++ h_task/main.py
@@ -0,0 +1,16 @@
+from src.widget import ask_account_card
+
+
+def main():
+    print(ask_account_card("Maestro 1596837868705199"))
+    print(ask_account_card("Счет 64686473678894779589"))
+    print(ask_account_card("MasterCard 7158300734726758"))
+    print(ask_account_card("Счет 35383033474447895560"))
+    print(ask_account_card("Visa Classic 6831982476737658"))
+    print(ask_account_card("Visa Platinum 8990922113665229"))
+    print(ask_account_card("Visa Gold 5999414228426353"))
+    print(ask_account_card("Счет 73654108430135874305"))
+
+
+if __name__ == "__main__":
+    main()
