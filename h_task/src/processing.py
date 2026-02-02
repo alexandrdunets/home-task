7dcc0f0d175e1f3221ff939c603e7661cecf759f
Alexandr Dunets <dunetsa@yandex.ru>
2026-02-02 19:57:12 +0300

    fix function filter_by_state()

--- h_task/src/processing.py
+++ h_task/src/processing.py
@@ -1,2 +1,13 @@
-def filter_by_state():
-    ...
+def filter_by_state(list_dict=None, state: str = "EXECUTED") -> list[dict]:
+    """Функция принимает список словарей и опционально значение для ключа state (по умолчанию 'EXECUTED'),
+       а возвращает новый список словарей, содержащий только те словари, у которых ключ state
+       соответствует указанному значению."""
+
+    list_res_dict = []
+    for dictionary in list_dict:
+        state_val = dictionary.get("state", 0)
+        if state_val == state:
+            list_res_dict.append(dictionary)
+    return list_res_dict
+
+
