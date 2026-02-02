cb6debe6cedab1663af1f2016c5a81b3524fc07c
Alexandr Dunets <dunetsa@yandex.ru>
2026-02-02 20:32:24 +0300

    Add function sort_by_date()

--- h_task/src/processing.py
+++ h_task/src/processing.py
@@ -11,3 +11,8 @@ def filter_by_state(list_dict=None, state: str = "EXECUTED") -> list[dict]:
     return list_res_dict
 
 
+def sort_by_date(list_dict=None, ascending: bool = False) -> list[dict]:
+    """Функция возвращает новый список, отсортированный по дате (date). На вход принимаются список словарей и
+        булевский параметр ascending (True -сортировка по возрастанию, по умолчанию сортировка производится по убыванию)"""
+    sorted_list = sorted(list_dict, key=lambda dictionary: dictionary.get("data", 0), reverse=ascending)
+    return sorted_list
