f92946a55e3402a7e422c6d90137851fff060766
Alexandr Dunets <dunetsa@yandex.ru>
2026-03-14 11:19:44 +0300

    Корректировка кода декоратора log()

--- h_task/src/decorators.py
+++ h_task/src/decorators.py
@@ -4,7 +4,8 @@ from typing import Optional
 
 def log(filename: Optional[str] = None):
     """Декоратор логирует имя функции и результат выполнения при успешной операции, а также
-    имя функции, тип возникшей ошибки и входные параметры, если выполнение функции привело к ошибке."""
+    имя функции, тип возникшей ошибки и входные параметры, если выполнение функции привело к ошибке.
+    Если параметр filename=file задан, логи записываются в указанный файл, иначе логи выводятся в консоль."""
 
     def decorator(func):
         @wraps(func)
@@ -12,23 +13,22 @@ def log(filename: Optional[str] = None):
 
             try:
                 result = func(*args, **kwargs)
-            except Exception as e:
-                if filename:
-                    with open(filename, 'a', encoding='utf-8') as f:
-                        f.write(f"{func.__name__} error: {str(e)}. Inputs: {args}, {kwargs}\n")
-                else:
-                    print(f"{func.__name__} error: {str(e)}")
-                raise Exception(f"{func.__name__} error: {str(e)}. Inputs: {args}, {kwargs}")
-
-            else:
                 if filename:
                     with open(filename, 'a', encoding='utf-8') as f:
                         f.write(f"{func.__name__} ok\n")
                 else:
                     print(f"{func.__name__} ok")
+                return result
 
-            return result
+            except Exception as e:
+                if filename:
+                    with open(filename, 'a', encoding='utf-8') as f:
+                        f.write(f"{func.__name__} error: {str(e)}. Inputs: {args}, {kwargs}\n")
+                else:
+                    print(f"{func.__name__} error: {str(e)}. Inputs: {args}, {kwargs}")
+                raise
 
         return wrapper
 
     return decorator
+
