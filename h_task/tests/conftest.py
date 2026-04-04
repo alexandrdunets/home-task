f6b65ad38c180898c34e770a2cd55bd9fe4e7846
Alexandr Dunets <dunetsa@yandex.ru>
2026-04-04 08:27:40 +0300

    fix conftest.py

--- h_task/tests/conftest.py
+++ h_task/tests/conftest.py
@@ -466,3 +466,24 @@ def card_number_generator_output():
         "0000 0000 0000 0004",
         "0000 0000 0000 0005",
     ]
+
+
+@pytest.fixture
+def json_to_list_input_output():
+    return [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        },
+    ]
