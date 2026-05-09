0894bcd09fb45f7b691f21372e7b5a2389b7d90d
Alexandr Dunets <dunetsa@yandex.ru>
2026-05-09 13:49:04 +0300

    Создание логов для функций модуля masks.py

--- h_task/src/masks.py
+++ h_task/src/masks.py
@@ -1,5 +1,21 @@
 from typing import Optional
 
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    filemode="w"
+)
+file_handler = logging.FileHandler(
+    filename="../h_task/logs/masks.log", encoding="utf-8"
+)
+file_formatter = logging.Formatter(
+    "%(asctime)s %(name)s: %(levelname)s %(message)s"
+)
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
 
 def get_mask_card_number(card_number: Optional[int | str] = None) -> str:
     """Функция get_mask_card_number принимает на вход номер карты
@@ -13,21 +29,27 @@ def get_mask_card_number(card_number: Optional[int | str] = None) -> str:
     7000 79** **** 6361  # выход функции"""
 
     if not card_number:
+        logger.error("Пользователь не ввел номер карты")
         raise ValueError("Номер карты отсутствует")
 
     if not isinstance(card_number, int | str):
+        logger.error("Тип данных входного аргумента должен быть целым или строковым")
         raise TypeError("Тип данных входного аргумента должен быть целым или строковым")
 
     mask_card_number = ""
     number_str = str(card_number)
 
     if not number_str.isdigit():
+        logger.error("Номер карты должен содержать только цифры")
         raise ValueError("Номер карты должен содержать только цифры")
 
     if len(number_str) != 16:
+        logger.error("Номер карты должен содержать 16 цифр")
         raise ValueError("Номер карты должен содержать 16 цифр")
 
     mask_card_number = number_str[:4] + " " + number_str[4:6] + "** **** " + number_str[-4:]
+
+    logger.info("Маска карты успешно сформирована")
 
     return mask_card_number
 
@@ -42,19 +64,25 @@ def get_mask_account(account_number: Optional[int | str] = None) -> str:
     **4305  # выход функции"""
 
     if not account_number:
+        logger.error("Пользователь не ввел номер счета")
         raise ValueError("Номер счета отсутствует")
 
     if not isinstance(account_number, int | str):
+        logger.error("Тип данных входного аргумента должен быть целым или строковым")
         raise TypeError("Тип данных входного аргумента должен быть целым или строковым")
 
     mask_account = ""
     account_str = str(account_number)
 
     if not account_str.isdigit():
+        logger.error("Номер счета должен содержать только цифры")
         raise ValueError("Номер счета должен содержать только цифры")
     if len(account_str) != 20:
+        logger.error("Номер счета должен содержать 20 цифр")
         raise ValueError("Номер счета должен содержать 20 цифр")
 
     mask_account = "**" + account_str[-4:]
+
+    logger.info("Маска счета успешно сформирована")
 
     return mask_account
